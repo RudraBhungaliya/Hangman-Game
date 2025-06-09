21f92ea3e55efca3030878a1913ea14491588574
Rudra Bhungaliya <133981939+RudraBhungaliya@users.noreply.github.com>
2025-06-09 17:46:14 +0530

    Update keyboard.tsx

--- src/keyboard.tsx
+++ src/keyboard.tsx
@@ -31,12 +31,17 @@ export default function Keyboard({
         const isActive = activeLetters.includes(key)
         const isInactive = inactiveLetters.includes(key)
 
+        let btnStyle = {}
+        if(isActive) btnStyle = { backgroundColor: "lightskyblue", color: "black" }
+        else if(isInactive) btnStyle = { backgroundColor: "#ff4d4d", color: "white" }
+
         return (
           <button
             key={key}
             onClick={() => addGuessedLetter(key)}
-            className={`btn ${isActive ? "active" : ""} ${isInactive ? "inactive" : ""}`}
+            className="btn"
             disabled={isActive || isInactive || disabled}
+            style={btnStyle}
           >
             {key}
           </button>
